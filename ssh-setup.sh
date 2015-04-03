15e2b91362b563e3f0be85385226ae9477ea6621
Christopher Dobler <chrisdobler@gmail.com>
2015-04-03 02:47:12 -0700

    Initial concept for ssh deployment
    -generates rsa
    -creates .ssh dir
    -inserts key to server
    -adds .ssh/config configuration
    -creates .aliases link

---
+++ ssh-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# SSH-SETUP by Christopher Dobler 2015
+#
+# USAGE:  
+#   ssh-setup user host friendly
+
+#create ssh key
+echo "Generating ssh key for $2"
+echo "User Name: $1"
+echo "Friendly Name: $3"
+var=`ssh-keygen -t rsa -f ~/.ssh/$2_rsa`
+echo "$var"
+
+echo ""
+echo "Creating .ssh directory on remote server"
+echo `ssh $1@$2 "mkdir ~/.ssh;"`
+
+echo ""
+echo "Inserting new key into remote server"
+echo `cat ~/.ssh/$2_rsa.pub | ssh $1@$2 "cat >> ~/.ssh/authorized_keys"`
+
+echo ""
+echo "Adding quick access alias to new remote"
+
+if hash gsed 2>/dev/null; then
+        echo `gsed -i "1s/^/alias $3='ssh -2 -p 22 $1@$2' \n/" ~/.aliases`
+        echo `gsed -i "1s/^/Host $2\n	IdentityFile ~\/.ssh\/$2_rsa\n/" ~/.ssh/config`
+else
+        echo `sed -i "1s/^/alias $3='ssh -2 -p 22 $1@$2' \n/" ~/.aliases` 
+        echo `sed -i "1s/^/Host $2\n   IdentityFile ~\/.ssh\/$2_rsa" ~/.ssh/config`
+fi
